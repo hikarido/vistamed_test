6fb9ea509b1054e2394bfb5cdf0d406884fc8554
Hikarido <kostyaananiev@gmail.com>
2017-11-08 19:20:36 +0300

    added Settings window 	modified:   MainWindow.py create settings window 	new file:   SettingsWindow.py not changet. just new lines 	modified:   Table.py ui for SettingsWindow 	new file:   dialog.ui

---
+++ SettingsWindow.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  8 18:20:02 2017
+
+@author: takava
+"""
+
+from PyQt4 import QtCore, QtGui, uic
+
+class SettingsWindow(QtGui.QDialog):
+    def __init__(self, parent= None):
+        QtGui.QDialog.__init__(self)
+        uic.loadUi("/home/takava/Forge/vistamed_test/dialog.ui", self)
+        self.setWindowTitle("Settings")
+        self.set_signals()
+        
+    def set_signals(self):
+        self.end_dialog.clicked.connect(self.close_dialog)
+        
+    def close_dialog(self):
+        print('closed')
+        self.hide()
+        
+    def get_settings(self):
+        data = dict()
+        for i in range(0, self.gridLayout.rowCount()):
+            key = self.gridLayout.itemAtPosition(i, 0).widget().text()            
+            value = self.gridLayout.itemAtPosition(i, 1).widget().text();
+            data[key] = value
+        return data
+        
+        
+if __name__ == '__main__':
+    print('ok')
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    
+    dialog = SettingsWindow()
+    dialog.show()
+    
+    sys.exit(app.exec_())
